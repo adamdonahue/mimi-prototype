b7393b6cc3ee4667fe0eb1870d6ebdded69a9ba9
Adam Donahue <adam.donahue@gmail.com>
2015-06-20 21:25:53 -0400

    Add landing pages for each subsection.

--- mimi/pages/urls.py
+++ mimi/pages/urls.py
@@ -4,4 +4,12 @@ urlpatterns = patterns(
     '',
 
     url('^$', 'pages.views.index', name='index'),
+
+    url('^acne-awareness/$', 'pages.views.acne_awareness', name='pages-acne-awareness'),
+    url('^celeb/$', 'pages.views.celeb', name='pages-celeb'),
+    url('^hair/$', 'pages.views.hair', name='pages-hair'),
+    url('^nails/$', 'pages.views.nails', name='pages-nails'),
+    url('^makeup/$', 'pages.views.makeup', name='pages-makeup'),
+    url('^skin/$', 'pages.views.skin', name='pages-skin'),
+
 )
--- mimi/pages/views.py
+++ mimi/pages/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pages/index.html')
+
+def acne_awareness(request):
+    return render(request, 'pages/acne-awareness.html')
+
+def celeb(request):
+    return render(request, 'pages/celeb.html')
+
+def hair(request):
+    return render(request, 'pages/hair.html')
+
+def nails(request):
+    return render(request, 'pages/nails.html')
+
+def makeup(request):
+    return render(request, 'pages/makeup.html')
+
+def skin(request):
+    return render(request, 'pages/skin.html')

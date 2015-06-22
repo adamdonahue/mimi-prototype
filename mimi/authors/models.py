72b0d3f8a5a597403a55837aa39c5cb215614d3f
Adam Donahue <adam.donahue@gmail.com>
2015-06-22 18:36:58 -0400

    Add comments.

--- mimi/authors/models.py
+++ mimi/authors/models.py
@@ -3,7 +3,11 @@ from django.db import models
 from django_extensions.db.fields import AutoSlugField
 
 class Author(models.Model):
+    """An author (user) on the Mimi site.  In theory all
+    registered users are (potential) authors, but some may
+    not have actually written anything.
 
+    """
     user = models.OneToOneField(settings.AUTH_USER_MODEL, unique=True)
     display_name = models.CharField(max_length=100)
     slug = AutoSlugField(unique=True, populate_from=('display_name',))
